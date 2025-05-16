00162699a6b7e5d6e181e66e712bb54450b34323
guilherme <guilhermelobobrayner@gmail.com>
2025-05-16 11:28:56 -0300

    add await

--- src/controller/canalController.js
+++ src/controller/canalController.js
@@ -3,7 +3,7 @@ import Canal from "../models/canalModel.js";
 
 export const getCanal = async (req, res) => {
   try {
-    const todosCanais = Canal.findAll();
+    const todosCanais = await Canal.findAll();
     res.status(200).json(todosCanais);
   } catch (error) {
     res.status(500).json("erro ao buscar os canais, contate o responsável");
--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 
 export const getUsers = async (req, res) => {
   try {
-    const allUsers = User.findAll();
+    const allUsers = await User.findAll();
     res.status(200).json(allUsers);
   } catch (error) {
     res.status(500).json("erro ao listar usuarios, contate o responsável");
